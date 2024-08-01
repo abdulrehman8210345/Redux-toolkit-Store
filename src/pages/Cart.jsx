4024f6c5801ca26b336619ef089b52a1f4485e3a
abdulrehman8210345 <abdulrehman8210345@gmail.com>
2024-08-01 20:21:26 +0500

    Cart functionalites added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { remove } from '../store/cartSlice';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const dispatch =useDispatch();
 
-export default Cart
+  const products= useSelector((state)=>state.cart);
+  const handleRemove= (Product_id)=>{
+      dispatch(remove(Product_id))
+  }
+  return (
+    <div>
+        <h3>Cart</h3>
+        <div className="cartWrapper">
+            {products.map((product) => (
+                <div key={product.id} className="cartCard">
+                    <img src={product.image} alt="" />
+                    <h5>{product.title}</h5>
+                    <h5>{product.price}</h5>
+                    <p>Quantity: {product.quantity}</p>
+                    <button
+                        className="btn"
+                        onClick={() => handleRemove(product.id)}
+                    >
+                        Remove
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
+);
+};
+
+export default Cart;
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,15 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const cartSlice = createSlice({
     name:'Cart',
     initialState:[],
     reducers:{
         add(state,action){
-            state.push(action.payload)
+            const existingProduct = state.find((item)=>item.id === action.payload.id);
+            if(existingProduct){
+                existingProduct.quantity+=1;
+            }
+            else{
+                state.push({...action.payload,quantity:1})
+            }
         },
         remove(state,action){
-            state.filter((item)=>item.id !== action.payload)
+            const index= state.findIndex((item)=>item.id===action.payload)
+            if(index !== -1) {
+                if(state[index].quantity > 1)
+                state[index].quantity -=1;
+                else{
+                    state.splice(index,1);
+                }
+            }
         }
     }
 })
